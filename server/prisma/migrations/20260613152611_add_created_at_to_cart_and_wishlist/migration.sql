5bf9064eb772f483ef67be5c20c2c993b418695c
Chayan Sehgal <chayansehgal2971@gmail.com>
2026-06-13 20:56:42 +0530

    schema-update

---
+++ server/prisma/migrations/20260613152611_add_created_at_to_cart_and_wishlist/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "cart_items" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "wishlist_items" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
